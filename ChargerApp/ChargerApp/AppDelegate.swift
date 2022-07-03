469c1626ca7284565b3f1ffd721b6fba8801ae91
Semra <karadogansemra@gmail.com>
2022-07-03 18:23:42 +0300

    Change the comment line

--- ChargerApp/ChargerApp/AppDelegate.swift
+++ ChargerApp/ChargerApp/AppDelegate.swift
@@ -13,7 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        Thread.sleep(forTimeInterval: 3.0) // Delay launch screen 3 seconds
+        // Delay launch screen 3 seconds
+        Thread.sleep(forTimeInterval: 3.0)
         return true
     }
 
