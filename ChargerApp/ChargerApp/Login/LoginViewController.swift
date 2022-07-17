05cf65852f2495bc84ad4e26cb7e7e02f87c0398
Semra <karadogansemra@gmail.com>
2022-07-17 15:27:38 +0300

    Appointment view controller and assets added

--- ChargerApp/ChargerApp/Login/LoginViewController.swift
+++ ChargerApp/ChargerApp/Login/LoginViewController.swift
@@ -21,6 +21,8 @@ class LoginViewController: UIViewController {
             loginAPI.fetchLogin(email: epostaTextField.text ?? "",udid: UDID ?? "")
         }else{
             print("Eposta is invalid!")
+            self.toastMessage("Eposta is invalid!")
+
         }
     }
     override func viewDidLoad() {
@@ -90,4 +92,36 @@ extension UIColor {
            blue: rgb & 0xFF
        )
    }
+}
+
+
+extension UIViewController {
+  func toastMessage(_ message: String){
+    guard let window = UIApplication.shared.keyWindow else {return}
+    let messageLbl = UILabel()
+    messageLbl.text = message
+    messageLbl.textAlignment = .center
+    messageLbl.font = UIFont.systemFont(ofSize: 12)
+    messageLbl.textColor = .white
+    messageLbl.backgroundColor = UIColor(white: 0, alpha: 0.5)
+
+    let textSize:CGSize = messageLbl.intrinsicContentSize
+    let labelWidth = min(textSize.width, window.frame.width - 40)
+
+    messageLbl.frame = CGRect(x: 20, y: window.frame.height - 90, width: labelWidth + 30, height: textSize.height + 20)
+    messageLbl.center.x = window.center.x
+    messageLbl.layer.cornerRadius = messageLbl.frame.height/2
+    messageLbl.layer.masksToBounds = true
+    window.addSubview(messageLbl)
+
+    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+
+    UIView.animate(withDuration: 3, animations: {
+        messageLbl.alpha = 0
+    }) { (_) in
+        messageLbl.removeFromSuperview()
+    }
+    }
+  }
+    
 }
